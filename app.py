988725a5288350aec1517db698d5dbd46e0191a6
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-16 11:10:29 -0400

    codealong with Rome

---
+++ app.py
@@ -0,0 +1,88 @@
+python_is_cool = True
+javascript_is_better = False  
+
+# List
+foods = ['burger', 'cheese', 'buns']
+print(foods[1])
+print(foods[-2])
+
+print('------')
+
+for food in foods:
+  print(food)
+
+
+print(len(foods))
+
+cars = ['Mercedes', 'Audi', 'Jaguar']
+for i, car in enumerate(cars):
+
+
+
+
+ teams = ['Cigars', 'Bucks', 'Nets', 'Jazz']
+ for i in range(len(teams)):
+  each_team = teams[i]
+  print('{}. {}'.format(i, each_team))
+
+  print('------')
+
+# Dictionaries
+student = {
+ "name": 'John',
+ "class": 'Physics',
+ "year": '2021'
+}
+
+# John
+print(student["name"])
+# Physics
+print(student.get('class'))
+
+print('----')
+
+# Iterate through a dictionary
+for key in student:
+ print('{}, {}'.format(key, student[key]))
+
+print('------')
+
+# Conditionals
+age = 22
+
+
+if age < 21:
+ print('Not old enough')
+elif age == 21:
+ print('You made it')
+else:
+ print("You're old enough")
+
+print('----')
+
+if student['name']:
+ print('{}'.format(student['name']))
+
+if 'Jesse' in student.get('name'):
+
+ print(True)
+else:
+ print(False)
+
+print('------')
+
+if 'Porche'in cars:
+ print(True)
+
+else:
+ print(False)
+
+def intro_student(name, course = 'Calculus', year = 2022):
+ print('My name is {}. I am in {} class. I graduate in {}'.format(name, course, year))
+
+# intro_student(student['name'], student['class'], student['year'])
+
+intro_student('Jesse', year=2007)
+
+
+
